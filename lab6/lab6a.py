e0f2abc2d81dea48aaaeb07c7c3725a0b8c6f73e
Liam Andersson <liaan514@student.liu.se>
2021-11-02 20:50:52 +0100

    Updated code

--- lab6/lab6a.py
+++ lab6/lab6a.py
@@ -4,13 +4,12 @@ def exec_program(arg):  #[calc,STATEMENTS]
         #Vi vet att det är ett program!
         statements = calc.program_statements(arg)
 
-        print(statements)
         if(calc.is_statements(statements)):
-            #Det finns flera statements
+            """Det finns flera statements eller endast ett då innehållet i  
+            statements kommer se ut som följande oavsett: [[exp1],[exp2]] / [[exp1]] """
+
             exec_statements(statements)
-        elif(calc.is_statement(statements)):
-            #Det finns bara ett statement
-            exec_statement(statements)
+        
         
         #Behövs nog inte
         return True
@@ -22,14 +21,38 @@ def exec_program(arg):  #[calc,STATEMENTS]
 def exec_statements(statements):
     #Gå igenom alla statements och kalla exec_statement för varje.
     #[1,2,3,[1,2,[3]]] skulle kunna vara statements (fast med statements istället för siffror)
-    #Vi måste alltså göra rekursion.
+    
     print("Statements!")
-
-    pass
+ 
+    for statement in statements:
+        #Här behöver vi kalla exec_statement som sedan kollar om det är selection,assignment etc...
+        exec_statement(statement)
     
 
 def exec_statement(statement):
+    #Kolla vilken typ av statement det är och kalla sedan eval_condition för att i t.ex selection kolla om det blir sant eller falskt
     print("Statement!")
 
+def eval_condition(expression):
+    if(calc.is_condition(expression)):
+        #Vi vet att det är ett condition, dvs att operatorn är en condoper
+        left = calc.condition_left(expression)
+        right = calc.condition_right(expression)
+        operator = calc.condition_operator(expression)
+        
+        if(operator == "<"):
+            return left < right
+        elif(operator == ">"):
+            return left > right
+        else:
+            return left == right
+        
+    #Annars kasta error
 
-print(exec_program(['calc', ['if', [3, '>', 5], ['print', 2]]]))
+def eval_binary(expression):
+    #Samma sak som i condition...
+    pass
+
+
+
+exec_program(['calc', ['if', [3, '>', 5], ['print', 2], ['print', 4]]])
