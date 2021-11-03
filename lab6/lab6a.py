8c6bca899afde7a5d1328320d35fc3f0386f323e
Liam Andersson <liaan514@student.liu.se>
2021-11-03 22:23:19 +0100

    Selection is mostly done, encountered some future problems that we will have to discuss during Lab2. Added more comments for easier reading.

--- lab6/lab6a.py
+++ lab6/lab6a.py
@@ -5,83 +5,115 @@ def exec_program(arg):  #[calc,STATEMENTS]
         statements = calc.program_statements(arg)
 
         if(calc.is_statements(statements)):
-            """Det finns flera statements eller endast ett då innehållet i  
-            statements kommer se ut som följande oavsett: [[exp1],[exp2]] / [[exp1]] """
 
             exec_statements(statements)
-        
-        
-        #Behövs nog inte
-        return True
-    
-    #Behövs nog inte
-    return False
-        
-
-def exec_statements(statements):
-    #Gå igenom alla statements och kalla exec_statement för varje.
-    #[1,2,3,[1,2,[3]]] skulle kunna vara statements (fast med statements istället för siffror)
-    
-    print("Statements!")
  
-    for statement in statements:
-        #Här behöver vi kalla exec_statement som sedan kollar om det är selection,assignment etc...
-        exec_statement(statement)
+def exec_statements(statements):
+
+    #Rekursion
+    if(not calc.empty_statements(statements)):
+        #Gå igenom alla statements
+        first = calc.first_statement(statements)
+        rest = calc.rest_statements(statements)
+
+        exec_statement(first)
+        exec_statements(rest)
     
 
 def eval_condition(expression) -> bool:
 
-    #Vi vet att det är ett condition, dvs att operatorn är en condoper, det kommer alltså alltid att returneras något
+    """Vad händer om man har [[5, + ,[5 + 5]],=,15] ?? """
+
+    #Vi vet att det är ett condition, dvs att operatorn är en condoper, det kommer alltså alltid att returneras något av följande
     left = calc.condition_left(expression)
+    #Kolla om left är ett binary expression?
     right = calc.condition_right(expression)
+    #Kolla om right är ett binary expression?
     operator = calc.condition_operator(expression)
     
     if(operator == "<"):
         return left < right
 
-    if(operator == ">"):
+    elif(operator == ">"):
         return left > right
 
     else:
         return left == right
 
 def eval_binary(expression):
-    #Samma sak som i condition...
-    pass
 
+    #Vi vet att det är ett binary expression, dvs att operatorn är en binary operator, det kommer alltså alltid att returneras något av följande
+    left = calc.binaryexpr_left(expression)
+    #Kolla om left är ett binary expression?
+    right = calc.binaryexpr_right(expression)
+    #Kolla om right är ett binary expression?
+    operator = calc.binaryexpr_operator(expression)
+    
+    if(operator == "+"):
+        return left + right
+
+    elif(operator == "-"):
+        return left - right
+
+    elif(operator == "*"):
+        return left * right
+    
+    else:
+        return left / right
 
 def exec_statement(statement):
     #Kolla vilken typ av statement det är och kalla sedan eval_condition för att i t.ex selection kolla om det blir sant eller falskt
-    print("Statement!")
+    print(statement)
 
-    #Göra match här istället så det ser finare ut? Korta ner kod i efterhand.
+    #Göra match här istället så det ser finare ut?
+
+    #Selection
     if(calc.is_selection(statement)):
-        condition = calc.selection_condition(statement)
 
+        condition = calc.selection_condition(statement)
+        #Kolla om det faktiskt är ett condition / binary expression, annars går det ju inte!
         if(calc.is_condition(condition)):
+            #Kolla om expressionet är sant
             evaluation = eval_condition(condition)
 
+            #Om conditionet / binary expr. stämmer så hämtar vi true branchen annars false branchen.
             if(evaluation):
-                #Do true branch
-                pass
+                #True Branch!
+                true_branch = calc.selection_true_branch(statement)
+                #True branch kan vara ett till statement Typ som [set, 'a', 7], rekurera
+                exec_statement(true_branch)
             else:
-                #Do false branch
-                pass
-                
+                #False Branch!
+                false_branch = calc.selection_false_branch(statement)
+                #False branch kan vara ett till statement Typ som [set, 'a', 7], rekurera
+                exec_statement(false_branch)  
+    
         else:
+            #Kasta error!
             pass
 
-    elif(calc.calc.is_assignment(statement)):
+  
+    #Assignment
+    elif(calc.is_assignment(statement)):
+        #Får man använda ett dictionary som är globalt här eller hur ska man spara värden?
         pass
     
+
     elif(calc.is_repetition(statement)):
         pass
     
+
     elif(calc.is_output(statement)):
-        pass
+        expression = calc.output_expression(statement)
+        print(expression)
     
+
     elif(calc.is_input(statement)):
-        pass
+        #Får man använda ett dictionary som är globalt här eller hur ska man läsa värden?
+        expression = calc.input_variable(statement)
 
 
-exec_program(['calc', ['if', [3, '>', 5], ['print', 2], ['print', 4]]])
+
+exec_program(['calc', ['if', [6, '>', 5], ['print', 2], ['print', 4]]])
+
+#exec_program(['calc', ['if', [[10,"+",5], '>', 5], ['print', 2], ['print', 4]]])
