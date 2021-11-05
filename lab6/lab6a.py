2f97f63c71e40507156fd152bba9aa1c1cabded4
Liam Andersson <liaan514@student.liu.se>
2021-11-05 20:19:43 +0100

    Kommenterad kod och gjort den mer lättläst

--- lab6/lab6a.py
+++ lab6/lab6a.py
@@ -1,45 +1,201 @@
+#-- Imports --
 import calc
 import copy
-def exec_program(args : list, variables = {}):  #[calc,STATEMENTS]
+
+#-- Main functions --
+"""These functions are the core to the program. These functions call sub-functions called "Statement functions" and "Evaluation functions". """
+
+def exec_program(args : list, variables = {}) -> dict: 
+    """Runs a ConstCalc program, args. You can also use your own premade variables with the second parameter."""
+
+    #We know that it's a program as the first element is "calc"
     if(calc.is_program(args)):
-        #Vi vet att det är ett program!
+        
         statements = calc.program_statements(args)
 
+        #If the program has any statements, run the second function
         if(calc.is_statements(statements)):
-
+            
             return exec_statements(statements,variables)
-
         
-def exec_statements(statements : list, variables : dict):
-    
+def exec_statements(statements : list, variables : dict) -> dict:
+    """Execute the statements of the program, returns a (maybe) modified dictionary"""
+
+    #Create a copy of the variables in order to avoid a destructable function
     copy_variables = copy.deepcopy(variables)
 
-    #Rekursion
+    #If there are any statements left in statements, go through them recursively
     if(not calc.empty_statements(statements)):
-        #Gå igenom alla statements
+
+        #Grab the first element and the rest
         first = calc.first_statement(statements)
         rest = calc.rest_statements(statements)
-        
 
+        #Go through the first statement and re-run the function with the rest.
         copy_variables = exec_statement(first, copy_variables)
         return exec_statements(rest, copy_variables)
 
-    #Nu finns det inga fler statements. Vi är därför klara och vi ska returnera vårt dictionary
+    #There are no statements left, return the variables
     return copy_variables
 
-def eval_condition(expression : list, variables : dict) -> bool:
-    """Fixa calc_expression"""
-    #Vi vet att det är ett condition, dvs att operatorn är en condoper, det kommer alltså alltid att returneras något av följande
-    left = calc.condition_left(expression)
+def exec_statement(statement : list, variables : dict) -> dict:
+    """Executes a statement no matter what type it is, returns a (maybe) modified dictionary"""
+    #Make a copy in order to avoid a destructable function
+    copy_variables = copy.deepcopy(variables)
 
+    #Here we have distributed the code throughout multiple functions in order to make the code more readable.
+
+    #Selection
+    if(calc.is_selection(statement)):
+        copy_variables = exec_selection(statement,copy_variables)
+  
+    #Assignment
+    elif(calc.is_assignment(statement)):
+        copy_variables = exec_assignment(statement,copy_variables)
+
+    #Repetition
+    elif(calc.is_repetition(statement)):
+        copy_variables = exec_repetition(statement,copy_variables)
+    #Output
+    elif(calc.is_output(statement)):
+        exec_output(statement,copy_variables)
+    
+    #Input
+    elif(calc.is_input(statement)):
+        copy_variables = exec_input(statement,copy_variables)
+   
+    #Return the (maybe) modified copy
+    return copy_variables
+
+
+#-- Execute statement functions ()
+
+"""These functions are used within the exec_statement function"""
+
+def exec_repetition(statement : list, variables : dict) -> dict:
+    """Executes a repetition statement and returns a (maybe) modified dictionary"""
+    #Make a copy of the dictionary so that the function is not destructive
+    copy_variables = copy.deepcopy(variables)
+    #Grab the condition and the statements from the current statement
+    loop_condition = calc.repetition_condition(statement)
+    loop_statements = calc.repetition_statements(statement)
+    
+    #While the condition is true, execute the loop_statements
+    while eval_condition(loop_condition,copy_variables):
+        
+        #Return the new copy_variables
+        copy_variables = exec_statements(loop_statements, copy_variables)
+
+    return copy_variables
+
+def exec_selection(statement : list,variables : dict) -> dict:
+    """Executes a selection statement and returns a (maybe) modified dictionary"""
+
+    #Grab the selection condition
+    condition = calc.selection_condition(statement)
+
+    #Check if it's an actual condition, if not throw an error
+    if(calc.is_condition(condition)):
+        
+        #Use the eval_condition function (which returns if a condition is T/F) to see if the selection condition is true
+        evaluation = eval_condition(condition,variables)
+
+        #If it's true, get the true branch
+        if(evaluation):
+            #True Branch!
+            true_branch = calc.selection_true_branch(statement)
+            
+            #Execute and return the true_branch statement
+            return exec_statement(true_branch,variables)
+        else:
+            #Else, check if the statement has a false branch
+            if(calc.selection_has_false_branch(statement)):
+                #False Branch!
+                false_branch = calc.selection_false_branch(statement)
+                
+                #Execute and return the false_branch statement
+                return exec_statement(false_branch,variables)  
+
+    else:
+        #Kasta error!
+        pass
+    
+    #Return the  (maybe) modified copy_variables
+    return variables
+
+def exec_input(statement : list,variables : dict) -> dict:
+    """Executes an input statement and returns a modified dictionary"""
+
+    #Create a copy in order to avoid a destructable function
+    copy_variables = copy.deepcopy(variables)
+
+    #Grab the variable that is to be modified
+    variable = calc.input_variable(statement)
+    
+    #If the input is not an int, throw an error (Should floats be allowed too?)
+    try:
+        #Get the user input
+        user_input = int(input("Enter value for " + variable + ": "))
+        
+        #Update the dictionary
+        copy_variables[variable] = user_input
+
+        #Return the modified dictionary.
+        return copy_variables
+
+    except TypeError:
+        pass
+        #Throw error
+
+def exec_output(statement : list,variables : dict):
+    """Executes an output statement and does not return anything"""
+    #Grab the output_expression, which we will use if and only if it's a variable.
+    output_expression = calc.output_expression(statement)
+    expression = eval_expression(output_expression,variables)
+
+    #If it is a variable, print which variable is equal to the output.
+    if(calc.is_variable(output_expression)):
+        print(output_expression + " = " + str(expression))
+    else:
+        print(expression)
+
+def exec_assignment(statement : list, variables : dict) -> dict:
+    """Executes an assignment statement and returns a modified dictionary"""
+    #Create a copy in order to avoid the function from being destructive
+    variables_copy = copy.deepcopy(variables)
+
+    #Grab the variable and it's soon to be value
+    variable = calc.assignment_variable(statement)
+    value = eval_expression(calc.assignment_expression(statement),variables)
+    
+    #Check if the value is a constant, if not - cast an error
+    if(calc.is_constant(value)):
+        variables_copy[variable] = value
+        #Return the modified copy
+        return variables_copy
+
+    #Throw error
+
+
+#-- Evaluate functions --
+
+"""These functions evaluate conditions and expressions and are heavily used within the functions above."""
+
+def eval_condition(expression : list, variables : dict) -> bool:
+    """When we know that it in fact is a condition we can calculate it's value with this function"""
+
+    #Grab the left side of the expression and get it's "true" value
+    left = calc.condition_left(expression)
     left = eval_expression(left,variables)
 
+    #Grab the right side of the expression and get it's "true" value
     right = calc.condition_right(expression)
-
     right = eval_expression(right,variables)
 
+    #Get the operator
     operator = calc.condition_operator(expression)
     
+    #Return a different value depending on the operator
     if(operator == "<"):
         return left < right
 
@@ -50,15 +206,18 @@ def eval_condition(expression : list, variables : dict) -> bool:
         return left == right
 
 def eval_binary(expression : list, variables : dict) -> bool:
-    # [[5, +, 5], + ,5]
-    #Vi vet att det är ett binary expression, dvs att operatorn är en binary operator, det kommer alltså alltid att returneras något av följande
+    """When we know that it's a binary expression, we can use this function to calculate it's value"""
+    
+    #Grab left side of the expression
     left = eval_expression(calc.binaryexpr_left(expression),variables)
- 
+    
+    #Grab the right side of the expression
     right = eval_expression(calc.binaryexpr_right(expression),variables)
 
-    
+    #Grab the operator 
     operator = calc.binaryexpr_operator(expression)
     
+    #Return a different value depending on the operator
     if(operator == "+"):
         return left + right
 
@@ -71,99 +230,8 @@ def eval_binary(expression : list, variables : dict) -> bool:
     else:
         return left / right
 
-
-
-def exec_statement(statement : list, variables : dict):
-    #Kolla vilken typ av statement det är och kalla sedan eval_condition för att i t.ex selection kolla om det blir sant eller falskt
-
-    copy_variables = copy.deepcopy(variables)
-
-    #Selection
-    if(calc.is_selection(statement)):
-
-        condition = calc.selection_condition(statement)
-        #Kolla om det faktiskt är ett condition / binary expression, annars går det ju inte!
-        if(calc.is_condition(condition)):
-            #Kolla om expressionet är sant
-            evaluation = eval_condition(condition,copy_variables)
-
-            #Om conditionet / binary expr. stämmer så hämtar vi true branchen annars false branchen.
-            if(evaluation):
-                #True Branch!
-                true_branch = calc.selection_true_branch(statement)
-                #True branch kan vara ett till statement Typ som [set, 'a', 7], rekurera
-                copy_variables = exec_statement(true_branch,copy_variables)
-            else:
-
-                if(calc.selection_has_false_branch(statement)):
-                    #False Branch!
-                    false_branch = calc.selection_false_branch(statement)
-                    #False branch kan vara ett till statement Typ som [set, 'a', 7], rekurera
-                    copy_variables = exec_statement(false_branch,copy_variables)  
-    
-        else:
-            #Kasta error!
-            pass
-
-  
-    #Assignment
-    elif(calc.is_assignment(statement)):
-        copy_variables = exec_assignment(statement,copy_variables)
-
-    #Repetition
-    elif(calc.is_repetition(statement)):
-
-        loop_condition = calc.repetition_condition(statement)
-        loop_statements = calc.repetition_statements(statement)
-        
-        while eval_condition(loop_condition,copy_variables):
-            #print(copy_variables)
-            copy_variables = exec_statements(loop_statements, copy_variables)
-
-    #Output
-    elif(calc.is_output(statement)):
-        output_expression = calc.output_expression(statement)
-        expression = eval_expression(output_expression,copy_variables)
-
-        #Om det är en variabel så skriv ut det också
-        if(calc.is_variable(output_expression)):
-            print(output_expression + " = " + str(expression))
-        else:
-            print(expression)
-    
-    #Input
-    elif(calc.is_input(statement)):
-        #Numeriskt värde m.h.a input()
-        variable = calc.input_variable(statement)
-
-        try:
-            user_input = int(input("Enter value for " + variable + ": "))
-
-            copy_variables[variable] = user_input
-
-        except TypeError:
-            pass
-            #Throw error
-    
-    return copy_variables
-
-
-def exec_assignment(statement : list, variables : dir):
-    #Använd dictionary
-    variables_copy = copy.deepcopy(variables)
-
-    variable = calc.assignment_variable(statement)
-    value = eval_expression(calc.assignment_expression(statement),variables)
-    
-    #Kolla om det är ett numeriskt värde?
-    if(calc.is_constant(value)):
-        variables_copy[variable] = value
-
-        return variables_copy
-
-    #Throw error
-
 def eval_expression(expression : list, variables : dict):
+    """This function 'calculates' all expressions. It can: return a variables value, return the value of a binary expression, return a constant"""
     if(calc.is_binaryexpr(expression)):
         return eval_binary(expression,variables)
     
@@ -173,28 +241,35 @@ def eval_expression(expression : list, variables : dict):
     elif(calc.is_constant(expression)):
         return eval_constant(expression)
 
+    #If none of the above worked, throw an error as it isn't an expression
     #Throw error
 
-
 def eval_constant(expression):
+    """Returns the value of the constant"""
     return expression
 
 def eval_variable(expression,variables):
+    """Returns the variable's value"""
+    #Look up so that the variable in fact is in the dictionary before grabbing it's value.
     if(expression in variables.keys()):
         return variables[expression]
 
+    #If it's not in the dictionary, throw an error.
     #Throw error
+
+
+#-- Code to run the program --
 
 #exec_program(['calc', ['if', [6, '>', 5], ['print', 2], ['print', 4]]])
 
 #exec_program(['calc', ['if', [[5,"+",[5,"-",5]], '>', 5], ['print', 2], ['print', 4]]])
 
-#exec_program(['calc',["set","n",7], ["while", ["n",">",5],
-           # ["set","n",["n","-",1]],["print","n"]]])
+"""print(exec_program(['calc',["set","n",7], ["while", ["n",">",5],
+           ["set","n",["n","-",1]],["print","n"]]]))"""
 
-print(exec_program([
+"""print(exec_program([
         "calc",
         ["read", "x"],
         ["if", ["x", ">", 0], ["set", "a", 1], ["set", "a", -1]],
         ["if", ["x", "=", 0], ["set", "a", 0]],
-    ]))
+    ]))"""
