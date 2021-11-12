81cdbcbc449361e1b26436bb72fbb33d745ce34c
Liam Andersson <liaan514@student.liu.se>
2021-11-12 16:43:16 +0100

    Lab 7A done

--- lab7/lab7.py
+++ lab7/lab7.py
@@ -1,23 +1,43 @@
 from books import db
-from match import match
 
 #Deluppgift 1 - Utöka Match
-
-print(match([["hej",["hej1",["hej3"]],"hej2"],["hej",["hej2",["hej3"]],"hej2"]],["&",["hej1", ["&"]],"&"])) 
-
-
 def deep_match(seq : list, pattern : list):
-    """A pattern match algorithm that checks lists in lists"""
-    value = False
-    for i in seq:
+    """A pattern match algorithm"""
+    
+    if not pattern:
+        return not seq
+    
+    if pattern[0] == '--':
+        if deep_match(seq, pattern[1:]):
+            return True
+        elif not seq:
+            return False
+        else:
+            return deep_match(seq[1:], pattern)
+    elif not seq:
+        return False
+    elif pattern[0] == '&':
+        return deep_match(seq[1:], pattern[1:])
+    elif seq[0] == pattern[0]:
+        return deep_match(seq[1:], pattern[1:])
+    elif(isinstance(seq[0],list) and isinstance(pattern[0],list)):
+        return deep_match(seq[0],pattern[0]) and deep_match(seq[1:],pattern[1:])
+    else:
+        return False
+"""
+print(deep_match([[["hej1","hej2"],["hej2",["hej3"]]]],
+                 [["--",["hej2",["&"]]]])) 
+"""
 
-        #Det är en lista
-        if(isinstance(seq[i],list)):
-            deep_match(seq[i],pattern[i])
+def search(pattern : list, db : list):
+    """Seatch for a pattern in a database"""
+    if(not db):
+        return False
 
-        #Inte är en lista
-        value = match(seq[i])
+    if(not deep_match(db[0],pattern)):
+        return search(pattern,db[1:])
 
-    return value
+    return db[0]
 
+print(search(['--', ['titel', ['&', '&']], '--'], db))
 #Deluppgift 2 - Sök i databas
