2299b56fe900ab2949438e34287ac3f62a690fc0
Liam Andersson <liaan514@student.liu.se>
2021-11-30 13:23:15 +0100

    Lab8B complete, atleast we think so

--- lab8/lab8b.py
+++ lab8/lab8b.py
@@ -1,9 +1,12 @@
 # =========================================================================
 # Type definition
 # =========================================================================
-
 # Define the type somehow...  The initial "" is simply here as a placeholder.
-TimeSpanSeq = ""
+from typing import NamedTuple,List
+import copy
+from cal_abstraction import CalendarDay, Time, TimeSpan, ensure_type, new_time_span,Hour,Minute
+
+TimeSpanSeq = NamedTuple("TimeSpanSeq", [("sequences",List[TimeSpan])])
 
 # =========================================================================
 #  Function implementations
@@ -11,24 +14,48 @@ TimeSpanSeq = ""
 
 # Implement these functions!  Also determine if you need *additional* functions.
 
-def new_time_span_seq():
-    pass
+def new_time_span_seq(sequences : List[TimeSpan] = None) -> TimeSpanSeq:
+    """Create a new TimeSpanSeq"""
+    if sequences is None:
+        sequences = []
+    else:
+        ensure_type(sequences,List[TimeSpan])
+
+    return TimeSpanSeq(sequences)
+    
+
+def tss_is_empty(time_span_seq : TimeSpanSeq) -> bool:
+    """Returns true or false depending on if the TimeSpanSeq is empty or not"""
+    ensure_type(time_span_seq,TimeSpanSeq)
+
+    return not time_span_seq.sequences
+     
 
 
-def tss_is_empty(tss):
-    pass
+def tss_plus_span(time_span_seq, time_span):
+    """Adds a TimeSpan to a TimeSpanSequence, returns a new TimeSpanSequence"""
+    tss =  copy.deepcopy(time_span_seq)
+    ensure_type(time_span_seq,TimeSpanSeq) and ensure_type(time_span,TimeSpan)
+
+    tss.sequences.append(time_span)
+
+    return tss
 
 
-def tss_plus_span(tss, ts):
-    pass
+def tss_iter_spans(time_span_seq):
+    ensure_type(time_span_seq,TimeSpanSeq)
+
+    for span in time_span_seq.sequences:
+        yield span
 
 
-def tss_iter_spans(tss):
-    pass
+def show_time_spans(time_span_seq):
+    #Is it okay to iterate here?
+    """Outputs all the TimeSpans in TimeSpanSeq"""
+    generator = tss_iter_spans(time_span_seq)
 
-
-def show_time_spans(tss):
-    pass
+    for span in generator:
+        print(span)
 
 
 # Keep only time spans that satisfy pred.
@@ -40,3 +67,11 @@ def tss_keep_spans(tss, pred):
             result = tss_plus_span(result, span)
 
     return result
+
+
+#Test code
+tss = new_time_span_seq([new_time_span(Time(Hour(14),Minute(00)),Time(Hour(16),Minute(00)))])
+print(tss_is_empty(tss))
+tss = tss_plus_span(tss,new_time_span(Time(Hour(15),Minute(00)),Time(Hour(17),Minute(00))))
+
+show_time_spans(tss)
